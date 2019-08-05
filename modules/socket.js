557a4e561aca9402db6174de2d782dd6f742fd64
naimish1996 <naimishjazz009@gmail.com>
2019-08-05 18:28:28 +0530

    hello

---
+++ modules/socket.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+var server = require('../app').server;
+var io = require('socket.io')(server);
+
+router.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+module.exports = router;
